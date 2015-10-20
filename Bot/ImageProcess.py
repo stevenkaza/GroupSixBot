52f903c01c6581cda746a910aacd22660b5d948b
Steven Kazavchinski <Steven@Stevens-MacBook-Pro-2.local>
2015-10-19 23:09:41 -0400

    Succesfully converts image to grayscale

--- Bot/ImageProcess.py
+++ Bot/ImageProcess.py
@@ -1,7 +1,7 @@
 import sys
 
 from PIL import Image
-import numpy
+import numpy as np
 
 class ImageProcess:
 
@@ -12,19 +12,22 @@ class ImageProcess:
 		self.extention = splitName[1]
 		self.blackValue = 5
 
-	def process(self, name = "testPic", extention = "jpeg"):
+	def process(self, name = "testPic.jpeg", extention = "jpeg"):
 
 		im = Image.open(name)
 		white = 0
 		black = 0
 		#converting to grayscale using opencv
-		img = cv2.imread(name,0)
 
+		im=im.convert('L') #makes it greyscale
+		y=np.asarray(im.getdata(),dtype=np.float64).reshape((im.size[1],im.size[0]))
+		y=np.asarray(y,dtype=np.uint8) #if values still in range 0-255!
+		w=Image.fromarray(y,mode='L')
+		w.save('out.jpg')
 
 		'''
 		This just test for one colour: Black, anything that isn't black (define as a y value less than 5) is white (for now)
 
-		'''
 
 		for i in im.getdata():
 			if i[1] < self.blackValue:
@@ -36,3 +39,6 @@ class ImageProcess:
 		#print im.getcolors()
 		print "Size: ",im.size[0],im.size[1],"White: ",white,"Black: ",black
 		return {'white':white, 'black' : black}
+		'''
+ip = ImageProcess()
+ip.process()
